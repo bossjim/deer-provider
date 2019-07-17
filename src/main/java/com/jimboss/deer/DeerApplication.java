e01e610494804135e1ea62f1b182b7bf6a26b617
bossjim <17521335991@163.com>
2019-07-17 18:19:11 +0800

    权限管理系统后端第一版

---
+++ src/main/java/com/jimboss/deer/DeerApplication.java
@@ -0,0 +1,13 @@
+package com.jimboss.deer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeerApplication.class, args);
+    }
+
+}
