785e64dfda2414de012af015418308b48389fa54
bossjim <17521335991@163.com>
2019-07-19 15:08:32 +0800

    权限管理系统后端开发版

---
+++ src/main/java/com/jimboss/deer/system/controller/MenuController.java
@@ -0,0 +1,19 @@
+package com.jimboss.deer.system.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MenuController
+ * @Description TODO
+ * @Author jinyong
+ * @DATE 2019/7/18 12:08
+ * @Version 1.0
+ **/
+
+@Validated
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+}
